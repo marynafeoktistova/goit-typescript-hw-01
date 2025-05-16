549c63eed3931a2505c3f0f5a20bd34191e433eb
Maryna <tromsa3122@mail.com>
2025-05-16 15:16:17 +0300

    add console

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -1,2 +1,8 @@
 let unionTypeValue: string | number;
 let literalTypeValue: 'enable' | 'disable';
+
+unionTypeValue = 'Hello';
+literalTypeValue = 'enable';
+
+console.log(unionTypeValue);
+console.log(literalTypeValue);
